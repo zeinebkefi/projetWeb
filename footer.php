4d319bb6dfb79fdd6d853014621ce985d92340cd
ilef-malouch <78976103+ilef-malouch@users.noreply.github.com>
2021-04-27 00:02:31 +0200

    ilef

--- footer.php
+++ footer.php
@@ -1,20 +1,21 @@
+
 <footer class="footer">
 
     <section class="mb-4">
         <!-- Facebook -->
-        <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/" role="button"><img src="facebook.png" height="20px" width="20px"><i class="fab fa-facebook-f"></i
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/" role="button"><img src="image/facebook.png" height="20px" width="20px"><i class="fab fa-facebook-f"></i
             ></a>
 
         <!-- Twitter -->
-        <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/login?lang=fr" role="button"><img src="twitter.png" height="20px" width="20px"><i class="fab fa-twitter"></i
+        <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/login?lang=fr" role="button"><img src="image/twitter.png" height="20px" width="20px"><i class="fab fa-twitter"></i
             ></a>
 
         <!-- Google -->
-        <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/" role="button"><img src="youtube (1).png" height="20px" width="20px"><i class="fab fa-youtube"></i
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/" role="button"><img src="image/youtube.png" height="20px" width="20px"><i class="fab fa-youtube"></i
             ></a>
 
         <!-- Instagram -->
-        <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/?hl=fr" role="button"><img src="instagram.png" height="20px" width="20px"><i class="fab fa-instagram"></i
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/?hl=fr" role="button"><img src="image/instagram.png" height="20px" width="20px"><i class="fab fa-instagram"></i
             ></a>
 
     </section>
@@ -24,44 +25,40 @@
         <li>
             <h2>Product</h2>
             <ul class="box">
-                <li><a href="#"  style="color: white;">Theme Design</a></li>
-                <li><a href="#"  style="color: white;">Plugin Design</a></li>
-                <li><a href="#"  style="color: white;">Wordpress</a></li>
-                <li><a href="#"  style="color: white;">Joomla Yemplate</a></li>
-                <li><a href="#"  style="color: white;">Html Template</a></li>
+                <li><a href="#" style="color: white;">Theme Design</a></li>
+                <li><a href="#" style="color: white;">Plugin Design</a></li>
+                <li><a href="#" style="color: white;">Wordpress</a></li>
+                <li><a href="#" style="color: white;">Joomla Yemplate</a></li>
+                <li><a href="#" style="color: white;">Html Template</a></li>
             </ul>
         </li>
         <li class="features">
-            <h2>Useful Link</h2>
+
             <ul class="box">
-                <li><a href="#"  style="color: white;">Blog</a></li>
-                <li><a href="#"  style="color: white;">Pricing</a></li>
-                <li><a href="#"  style="color: white;">Sales</a></li>
-                <li><a href="#"  style="color: white;">Tickets</a></li>
-                <li><a href="#"  style="color: white;">Certifications</a></li>
-                <li><a href="#"  style="color: white;">Customer Services</a></li>
+                <li><img src="image/express-delivery.png"  width="30px" height="30px"> <h6 style="color: white"> livraison entre 3 et 5 jours</h6><br> </li>
+                <li><img src="image/payment-method.png"  width=30px" height="30px"> <h6 style="color: white"> paiement à la livraison</h6><br></li>
+                <li><img src="image/person.png"  width="30px" height="30px"><h6 style="color: white"> satisfait ou remboursé</h6><br></li>
             </ul>
         </li>
         <li>
             <h2>Address</h2>
             <ul class="box">
                 <li><a href="#" style="color: white;">127, Westwood Lane</a></li>
-                <li><a href="#"  style="color: white;">DA15 9PS</a></li>
-                <li><a href="#"  style="color: white;">London</a></li>
-                <li><a href="#"  style="color: white;">Unites Kingdom</a></li>
+                <li><a href="#" style="color: white;">DA15 9PS</a></li>
+                <li><a href="#" style="color: white;">London</a></li>
+                <li><a href="#" style="color: white;">Unites Kingdom</a></li>
             </ul>
         </li>
     </ul>
     </li>
     </ul>
     <div class="footer-bottom">
-        <p  style="color: white;">All Right reserved by &copy; conceptial 2021 </p>
+        <p style="color: white;">All Right reserved by &copy; conceptial 2021 </p>
     </div>
 
 </footer>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-</body>
-</html>
